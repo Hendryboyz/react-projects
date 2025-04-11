195d7d6fff6930c928f5528e809305d81e2b3a5e
Hendryboyz <hendry.js1247@gmail.com>
2025-04-11 16:17:28 +0800

    feat: combine active player and board state into game turns and show logs

--- 07-tic-tac-toe-starting-project/src/App.jsx
+++ 07-tic-tac-toe-starting-project/src/App.jsx
@@ -1,12 +1,28 @@
 import Player from "./components/Player.jsx";
 import GameBoard from "./components/GameBoard.jsx";
 import {useState} from "react";
+import Log from "./components/Log.jsx";
+
+const deriveActivePlayer = (gameTurns) => {
+  if (gameTurns.length > 0 && gameTurns[0].player === 'X') {
+    return 'O';
+  } else {
+    return 'X';
+  }
+}
 
 function App() {
-  const [activePlayer, setActivePlayer] = useState('X');
+  const [gameTurns, setGameTurns] = useState([]);
 
-  const handleSelectSquare = () => {
-    setActivePlayer((currentActivePlayer) => currentActivePlayer === 'X' ? 'O' : 'X');
+  const activePlayer = deriveActivePlayer(gameTurns);
+
+  const handleSelectCell = (rowId, colId) => {
+    setGameTurns(prevTurns => {
+      return [{
+        square: { row: rowId, col: colId },
+        player: deriveActivePlayer(prevTurns),
+      }, ...gameTurns];
+    });
   }
 
   return (
@@ -16,8 +32,9 @@ function App() {
           <Player initialName="Player 1" symbol="X" isActive={activePlayer === 'X'} />
           <Player initialName="Player 2" symbol="O" isActive={activePlayer === 'O'} />
         </ol>
-        <GameBoard onSelectCell={handleSelectSquare} player={activePlayer} />
+        <GameBoard onSelectCell={handleSelectCell} turns={gameTurns} />
       </div>
+      <Log turns={gameTurns} />
     </main>
   )
 }
--- 07-tic-tac-toe-starting-project/src/components/GameBoard.jsx
+++ 07-tic-tac-toe-starting-project/src/components/GameBoard.jsx
@@ -1,34 +1,31 @@
-import {useState} from "react";
-
 const initialGameBoard =  [
   [null, null, null],
   [null, null, null],
   [null, null, null],
 ];
 
-export default function GameBoard( { onSelectCell, player } ) {
-  const [currentGameBoard, setGameBoard] = useState(initialGameBoard);
-
-  const handleClickCell = (rowId, colId) => {
-    setGameBoard(prevGameBoard => {
-      const updatedBoard = [...prevGameBoard.map(innerArray => [...innerArray])];
-      updatedBoard[rowId][colId] = player;
-      return updatedBoard;
-    });
-
-    onSelectCell();
+export default function GameBoard( { onSelectCell, turns } ) {
+  // compute the game board from the turns state derived from App
+  let gameBoard = initialGameBoard;
+  for (const turn of turns) {
+    const { square, player } = turn;
+    const { row, col }  = square;
+    gameBoard[row][col] = player;
   }
 
   return (
     <ol id="game-board">
-      {currentGameBoard.map((row, rowId) => (
+      {gameBoard.map((row, rowId) => (
         <li key={rowId}>
           <ol>
             { row.map((playerSymbol, colId) =>
               <li key={colId}>
-                <button onClick={(event) => {
-                  handleClickCell(rowId, colId);
-                }}>
+                <button
+                  onClick={(_) => {
+                    onSelectCell(rowId, colId);
+                  }}
+                  disabled={playerSymbol !== null}
+                >
                   { playerSymbol }
                 </button>
               </li>
