de4476d708da3c3389721ce0adc2fc98b1b25f4c
Hendryboyz <hendry.js1247@gmail.com>
2025-04-14 16:20:13 +0800

    feat: apply zod to validate user input

--- 05-essentials-pratice/src/App.jsx
+++ 05-essentials-pratice/src/App.jsx
@@ -2,6 +2,13 @@ import {Fragment, useState} from 'react';
 import Header from "./components/Header.jsx";
 import UserInput from "./components/UserInput.jsx";
 import Result from "./components/Result.jsx";
+import { z } from 'zod';
+const Investment = z.object({
+  initialInvestment: z.number().positive('investment must greater than 0'),
+  annualInvestment: z.number().positive('investment must greater than 0'),
+  expectedReturn: z.number().positive('expected return must greater than 0'),
+  duration: z.number().positive('duration must greater than 1').min(1),
+});
 
 const initialInvestments = {
   initialInvestment: 10000,
@@ -19,11 +26,16 @@ function App() {
     })
   };
 
+  const { success: isValidInput, error} = Investment.safeParse(investments);
+  const errors = !isValidInput && error.errors;
+
   return (
     <Fragment>
       <Header title="Investment Caculator"/>
       <UserInput inputs={investments} handleChange={handleChange} />
-      <Result investments={investments} />
+      { isValidInput ? (<Result investments={investments} />) : (
+        errors.map((eachError => (<p className="center error-message">{eachError.message}</p>)))
+      ) }
     </Fragment>
   )
 }
