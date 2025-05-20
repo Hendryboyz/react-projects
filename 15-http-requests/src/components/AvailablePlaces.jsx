1f2ed5e096a618037603bf3626f85b5bdc311475
Hendryboyz <hendry.js1247@gmail.com>
2025-05-20 13:47:54 +0800

    feat: fetching the places data from backend with loading

--- 15-http-requests/src/components/AvailablePlaces.jsx
+++ 15-http-requests/src/components/AvailablePlaces.jsx
@@ -1,12 +1,42 @@
 import Places from './Places.jsx';
+import {useEffect, useState} from "react";
+import {flushSync} from "react-dom";
 
 export default function AvailablePlaces({ onSelectPlace }) {
+  const [availablePlaces, setAvailablePlaces] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+
+  // fetch retrieval
+  // useEffect(() => {
+  //   fetch('http://localhost:3000/places').then(response => {
+  //     return response.json();
+  //   }).then(data => {
+  //     setAvailablePlaces(data.places);
+  //   });
+  // }, []);
+
+  const fetchPlaces = async () => {
+    setIsLoading(true);
+    const resp = await fetch('http://localhost:3000/places');
+    const data = await resp.json();
+    setAvailablePlaces(data.places);
+    setIsLoading(false);
+  };
+
+  useEffect(() => {
+    // fetchPlaces().then(places => {
+    //   setAvailablePlaces(places);
+    // });
+    fetchPlaces();
+  }, []);
+
   return (
     <Places
       title="Available Places"
-      places={[]}
+      places={availablePlaces}
       fallbackText="No places available."
       onSelectPlace={onSelectPlace}
+      isLoading={isLoading}
     />
   );
 }
