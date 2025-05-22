0b26e57857118d222f2546c483087ee2d7544e4b
Hendryboyz <hendry.js1247@gmail.com>
2025-05-22 20:16:21 +0800

    feat: handle form value with state/ref

--- 17-form-user-input/src/components/Login.jsx
+++ 17-form-user-input/src/components/Login.jsx
@@ -1,24 +1,103 @@
-export default function Login() {
+import {useRef, useState} from "react";
+
+function StateLogin() {
+  const [data, setData] = useState({
+    email: "",
+    password: "",
+  });
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    console.log(data);
+  }
+
+  function handleInputChange(event) {
+    const { name, value } = event.target;
+    setData(prevValues => ({
+      ...prevValues,
+      [name]: value,
+    }))
+  }
+
+
   return (
-    <form>
+    <form onSubmit={handleSubmit}>
       <h2>Login</h2>
 
       <div className="control-row">
         <div className="control no-margin">
           <label htmlFor="email">Email</label>
-          <input id="email" type="email" name="email" />
+          <input
+            id="email"
+            type="email"
+            name="email"
+            value={data.email}
+            onChange={handleInputChange}
+          />
         </div>
 
         <div className="control no-margin">
           <label htmlFor="password">Password</label>
-          <input id="password" type="password" name="password" />
+          <input
+            id="password"
+            type="password"
+            name="password"
+            value={data.password}
+            onChange={handleInputChange}
+          />
         </div>
       </div>
 
       <p className="form-actions">
-        <button className="button button-flat">Reset</button>
-        <button className="button">Login</button>
+        <button type="reset" className="button button-flat">Reset</button>
+        <button type='submit' className="button">Login</button>
       </p>
     </form>
   );
 }
+
+function RefLogin() {
+  const emailRef = useRef();
+  const passwordRef = useRef();
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    console.log('submit!', emailRef.current.value, passwordRef.current.value);
+  }
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <h2>Login</h2>
+
+      <div className="control-row">
+        <div className="control no-margin">
+          <label htmlFor="email">Email</label>
+          <input
+            id="email"
+            type="email"
+            name="email"
+            ref={emailRef}
+          />
+        </div>
+
+        <div className="control no-margin">
+          <label htmlFor="password">Password</label>
+          <input
+            id="password"
+            type="password"
+            name="password"
+            ref={passwordRef}
+          />
+        </div>
+      </div>
+
+      <p className="form-actions">
+        <button type="reset" className="button button-flat">Reset</button>
+        <button type='submit' className="button">Login</button>
+      </p>
+    </form>
+  );
+
+}
+
+export default RefLogin;
