1c1d70a54ea4503fc15a465ec3b6b3788679917e
Hendryboyz <hendry.js1247@gmail.com>
2025-07-16 09:07:41 +0800

    feat: diff the scenario between initial and user search emtpy

--- 25-react-query/src/components/Events/FindEventSection.jsx
+++ 25-react-query/src/components/Events/FindEventSection.jsx
@@ -7,10 +7,13 @@ import EventItem from "./EventItem.jsx";
 
 export default function FindEventSection() {
   const searchElement = useRef();
-  const [searchTerm, setSearchTerm] = useState('');
-  const { data, isPending, isError, error } = useQuery({
+  const [searchTerm, setSearchTerm] = useState(undefined);
+  const isQueryEnabled = undefined !== searchTerm;
+  // isLoading = false, if the query is disabled but the isPending will be `true`
+  const { data, isLoading, isError, error } = useQuery({
     queryKey: ['events', { searchTerm: searchTerm }],
     queryFn: ({signal}) => fetchEvents({ signal, searchTerm }),
+    enabled: isQueryEnabled,
   });
 
   function handleSubmit(event) {
@@ -20,7 +23,7 @@ export default function FindEventSection() {
 
   let content = <p>Please enter a search term and to find events.</p>
 
-  if (isPending) {
+  if (isLoading) {
     content = <LoadingIndicator />;
   }
 
