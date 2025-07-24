506782189ed55652ac409827e5f99c7b9a4d67e6
Hendryboyz <hendry.js1247@gmail.com>
2025-07-24 10:26:52 +0800

    feat: add meal grid and item component to show the meal

--- 26-nextjs/foodies-project/app/meals/page.js
+++ 26-nextjs/foodies-project/app/meals/page.js
@@ -1,10 +1,23 @@
 import Link from "next/link";
+import classes from "./page.module.css";
 
 export default function MealsPage() {
   return (
-    <main>
-      <h1>Meals Page</h1>
-      <Link href="/meals/me">My Meal</Link>
-    </main>
+    <>
+      <header className={classes.header}>
+        <h1>
+          Delicious meals, create{' '}
+          <span>by you</span>
+        </h1>
+        <p>
+          Choose your favorite recipe and cook it yourself. It is easy and fun!
+        </p>
+        <p className={classes.cta}>
+          <Link href="/meals/share">Share Your Favorite Recipe</Link>
+        </p>
+      </header>
+      <main className={classes.main}>
+      </main>
+    </>
   );
 }
