fbdf42f20be132e7280b8883e289e2655d5e7ace
Hendryboyz <hendry.js1247@gmail.com>
2025-07-28 11:12:37 +0800

    feat: extract server action to submit shared meal

--- 26-nextjs/foodies-project/lib/meals.js
+++ 26-nextjs/foodies-project/lib/meals.js
@@ -1,4 +1,8 @@
+import fs from "node:fs";
+
 import sql from 'better-sqlite3';
+import slugify from "slugify";
+import xss from "xss";
 
 const db = sql('meals.db');
 
@@ -13,4 +17,34 @@ export async function getMeals() {
 
 export async function getMeal(slug) {
   return db.prepare(`SELECT * FROM meals WHERE slug = ?`).get(slug);
+}
+
+export async function saveMeal(meal) {
+  meal.slug = slugify(meal.title, { lower: true });
+  meal.instructions = xss(meal.instructions);
+
+  await writeImage(meal)
+  // @ symbol is support by `better-sqlite` package and the lib will extract the variable
+  // from the value object by the name you provided behind the @ symbol
+  // this way can allow to avoid SQL injection attack
+  db.prepare(`
+  INSERT INTO meals (title, summary, image, instructions, creator, creator_email, slug)
+  VALUES (@title, @summary, @image, @instructions, @creator, @creator_email, @slug)
+  `).run(meal);
+}
+
+async function writeImage(meal) {
+  const extension = meal.image.name.split('.').pop();
+  const fileName = `${meal.slug}.${extension}`
+
+  const stream = fs.createWriteStream(`public/images/${fileName}`);
+  const bufferedImage = await meal.image.arrayBuffer();
+  stream.write(Buffer.from(bufferedImage), (error) => {
+    if (error) {
+      throw new Error('Saving image failed!');
+    }
+  });
+
+  // all request to the image will be sent to public/ folder automatically
+  meal.image = `/images/${fileName}`;
 }
