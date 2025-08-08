9876e26f0da48215ff2416f2a081d4af12203fbb
Hendryboyz <hendry.js1247@gmail.com>
2025-08-08 10:06:45 +0800

    feat: use server action in client components

--- 27-server-side-components/app/page.js
+++ 27-server-side-components/app/page.js
@@ -1,13 +1,16 @@
 import RSCDemo from "@/components/RSCDemo";
 import ClientDemo from "@/components/ClientDemo";
+import DataFetchingDemo from "@/components/DataFetchingDemo";
+import ServerActionsDemo from "@/components/ServerActionsDemo";
 
 export default function Home() {
   return (
     <main>
-      <RSCDemo />
       <ClientDemo>
         <RSCDemo />
       </ClientDemo>
+      <DataFetchingDemo />
+      <ServerActionsDemo />
     </main>
   );
 }
