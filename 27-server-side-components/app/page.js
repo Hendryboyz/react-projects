35dc12b5b5278a865e4109959baf28fd9357ad9a
Hendryboyz <hendry.js1247@gmail.com>
2025-08-08 09:45:53 +0800

    feat: try to combine async server component into client component

--- 27-server-side-components/app/page.js
+++ 27-server-side-components/app/page.js
@@ -1,7 +1,13 @@
+import RSCDemo from "@/components/RSCDemo";
+import ClientDemo from "@/components/ClientDemo";
+
 export default function Home() {
   return (
     <main>
-      <p>Let's go!</p>
+      <RSCDemo />
+      <ClientDemo>
+        <RSCDemo />
+      </ClientDemo>
     </main>
   );
 }
---
+++ 27-server-side-components/components/ClientDemo.js
@@ -0,0 +1,25 @@
+'use client';
+
+import { useState } from 'react';
+import RSCDemo from "@/components/RSCDemo";
+
+export default function ClientDemo({ children }) {
+  const [count, setCount] = useState(0); // <- this is why it's a client component
+
+  console.log('ClientDemo rendered');
+  return (
+    <div className="client-cmp">
+      <h2>A React Client Component</h2>
+      <p>
+        Will be rendered on the client <strong>AND</strong> the server.
+      </p>
+      {/*<div>*/}
+      {/*  <button onClick={() => setCount(count + 1)}>*/}
+      {/*    + 1*/}
+      {/*  </button>*/}
+      {/*  <p>Count: {count}</p>*/}
+      {/*</div>*/}
+      {children}
+    </div>
+  );
+}
