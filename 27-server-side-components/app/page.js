a28c8de4a3e9f91ffa82e18afffcd74079bffff8
Hendryboyz <hendry.js1247@gmail.com>
2025-08-11 09:31:35 +0800

    feat: use error boundary to handle error in page router setup

--- 27-server-side-components/app/page.js
+++ 27-server-side-components/app/page.js
@@ -5,17 +5,24 @@ import ServerActionsDemo from "@/components/ServerActionsDemo";
 import UsePromiseDemo from "@/components/UsePromisesDemo";
 import {Suspense} from "react";
 import {fetchDummyUserData} from "@/actions";
+import ErrorBoundary from "@/components/ErrorBoundary";
 
 export default async function Home() {
-  const fetchUserPromise = new Promise(resolve => setTimeout(async () => {
-    const users = await fetchDummyUserData();
-    resolve(users);
+  const fetchUserPromise = new Promise((resolve, reject) => setTimeout(async () => {
+    try {
+      const users = await fetchDummyUserData();
+      resolve(users);
+    } catch (e) {
+      reject(new Error('Something went wrong!!'));
+    }
   }, 2000));
   return (
     <main>
-      <Suspense fallback={<p>Loading users...</p>}>
-        <UsePromiseDemo userPromise={fetchUserPromise}/>
-      </Suspense>
+      <ErrorBoundary>
+        <Suspense fallback={<p>Loading users...</p>}>
+          <UsePromiseDemo userPromise={fetchUserPromise}/>
+        </Suspense>
+      </ErrorBoundary>
       <ClientDemo>
         <RSCDemo />
       </ClientDemo>
