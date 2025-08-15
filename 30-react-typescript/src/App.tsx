d1790ede2c9c68ad7732076ee7d5953eaf55929a
Hendryboyz <hendry.js1247@gmail.com>
2025-08-15 10:00:51 +0800

    feat: useState() to manage todo items

--- 30-react-typescript/src/App.tsx
+++ 30-react-typescript/src/App.tsx
@@ -1,14 +1,23 @@
+import {useState} from "react";
 import './App.css'
 import Todos from "./components/Todo.tsx";
 import Todo from "./models/todo.ts";
+import NewTodo from "./components/NewTodo.tsx";
 
 function App() {
-  const todos = [
+  const [todos, setTodos] = useState<Todo[]>([
     new Todo('Learn React'),
     new Todo('Learn Typescript')
-  ];
+  ]);
+
+  function addTodoHandler(todoContent: string) {
+    setTodos((prevTodos) => {
+      return [...prevTodos, new Todo(todoContent)];
+    })
+  }
   return (
     <>
+      <NewTodo onAddTodo={addTodoHandler} />
       <Todos items={todos} />
     </>
   )
---
+++ 30-react-typescript/src/components/NewTodo.tsx
@@ -0,0 +1,32 @@
+import React, {useRef} from "react";
+
+type NewTodoProps = {
+  onAddTodo: (text: string) => void;
+};
+
+const NewTodo: React.FC<NewTodoProps> = (props) => {
+  const todoText = useRef<HTMLInputElement>(null);
+  function submitHandler(event: React.FormEvent) {
+    event.preventDefault();
+    if (!todoText.current) {
+      return;
+    }
+
+    // !(exclamation mark) means the null value is impossible
+    const enteredText = todoText.current!.value;
+    console.log(enteredText);
+    props.onAddTodo(enteredText);
+    todoText.current!.value = '';
+  }
+
+  return (
+    <form onSubmit={submitHandler}>
+      <label htmlFor="text">Todo Text</label>
+      <input type="text" id="text" ref={todoText} />
+
+      <button type="submit">Add Todo</button>
+    </form>
+  );
+};
+
+export default NewTodo;
