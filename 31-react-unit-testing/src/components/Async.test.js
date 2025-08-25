693b5386123eae70e7278398d7659d3fb5db9163
Hendryboyz <hendry.js1247@gmail.com>
2025-08-25 09:31:29 +0800

    feat: finish the async test

--- 31-react-unit-testing/src/components/Async.test.js
+++ 31-react-unit-testing/src/components/Async.test.js
@@ -1,1 +1,18 @@
-describe('Async component', () => {})
+import Async from "./Async";
+import {render, screen} from "@testing-library/react";
+
+describe('Async component', () => {
+  test('renders posts if request succeeds', async () => {
+    window.fetch = jest.fn();
+    window.fetch.mockResolvedValueOnce({
+      json: async () => { return [{ id: 'p1', title: 'First Post' }] },
+    })
+
+    render(<Async />);
+
+    // const listItemElements = screen.getAllByRole('listitem');
+    // find return a promise instead the elements itself
+    const listItemElements = await screen.findAllByRole('listitem');
+    expect(listItemElements).not.toHaveLength(0);
+  })
+})
