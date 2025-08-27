8e8821ed6e5a8289908d4a23a01ab9b16b46de1a
Hendryboyz <hendry.js1247@gmail.com>
2025-08-27 09:57:11 +0800

    feat: use the key to rerun the animate with react destroy/re-render properties

--- 32-react-animation/src/components/Challenges.jsx
+++ 32-react-animation/src/components/Challenges.jsx
@@ -45,6 +45,8 @@ export default function Challenges() {
           {displayedChallenges.length > 0 && (
             <motion.ol
               key="list" // key attrs require for multiple children in <AnimatePresence>
+              initial={{opacity: 0, y: -30}}
+              animate={{opacity: 1, y: 0}}
               exit={{ y: -30, opacity: 0 }}
               className="challenge-items"
             >
